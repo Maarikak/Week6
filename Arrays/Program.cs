902c10e252141adcb98b277cbb90ba3ddf4f7041
MaarikaK <testmaarika@gmail.com>
2023-12-14 23:59:33 +0200

    Week6

---
+++ Arrays/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+//Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+//fruitBasket[0] = "pine-apples";
+//fruitBasket[1] = "peaches";
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+//string[] fruitBasket = { "apples", "bananas", "oranges" };
+//Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+//for (int i = 0; i < fruitBasket.Length; i++)
+//{
+//    Console.WriteLine(fruitBasket[i]);
+//}
+
+//fruitBasket[0] = "pine-apples";
+//fruitBasket[1] = "peaches";
+
+//for (int i = 0; i < fruitBasket.Length; i++) 
+//{
+//    Console.WriteLine(fruitBasket[i]);
+//}
+
+
+//string hello = "Hello, world!".ToLower();
+//int counter = 0;
+//for (int i = 0; i < hello.Length; i++)
+//{
+//    if (hello [i] == 'l')
+//    {
+//counter++;
+//    }
+//}
+//Console.WriteLine($"There are {counter} l's in the string \"{hello}\"");
---
+++ Week6Strings and Arrays/Program.cs
@@ -0,0 +1,62 @@
+﻿
+
+//string mystring = "Hello, World! Have a wonderful day";
+
+//Console.WriteLine(mystring.Length);
+
+//using System;
+
+//Console.WriteLine("Enter your first name:");
+
+//string userName = Console.ReadLine().Trim();
+
+//Console.WriteLine(userName.Length);
+
+//string userName = "Maarika";
+
+//Console.WriteLine(userName.Length);
+//Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+//Console.WriteLine(userName[0]);
+//Console.WriteLine(userName[1]);
+//Console.WriteLine(userName[2]);
+//Console.WriteLine(userName[3]);
+//Console.WriteLine(userName[4]);
+////Console.WriteLine(userName[5]);
+////Console.WriteLine(userName[6]);
+
+//string userName = "Maarika";
+
+//Console.WriteLine(userName.Length);
+//Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+
+////a-counter
+
+////int counter = 0;
+
+////for (int i = 0; i < userName.Length; i++)
+////{
+////    if (userName[i] == 'a')
+////    { 
+////        counter++; //counter = counter + 1
+////    }
+////}
+
+////Console.WriteLine($"Your name has {counter} a-letters.");
+
+
+////replace
+////a-4
+
+//string updatedUserNamer = userName.Replace('a', '4').Replace('i', 'r');
+
+//Console.WriteLine(updatedUserNamer);
+
+
+//string hello = " hello";
+//Console.WriteLine(hello.Length);
+
+//string[] myNumbers = { "1, 2, 3", "4, 5, 6", "7, 8, 9" };
+//Console.WriteLine(myNumbers.Length);
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
